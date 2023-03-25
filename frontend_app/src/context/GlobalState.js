0c60d3d034be7ac58813048611413a115426a747
aherman21 <hermansalexander2@gmail.com>
2023-03-25 15:43:55 +0200

    Created GlobalProvider function

--- frontend_app/src/context/GlobalState.js
+++ frontend_app/src/context/GlobalState.js
@@ -1,5 +1,6 @@
 import React, {createContext, useReducer } from 'react'
 
+
 // Initital state
 
 const initialState = {
@@ -12,3 +13,14 @@ const initialState = {
 
 // Create context
 export const GlobalContext = createContext(initialState)
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
