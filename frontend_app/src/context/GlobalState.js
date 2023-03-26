52bd4f2dd94f5754b26776979820a705f32815a1
aherman21 <hermansalexander2@gmail.com>
2023-03-26 17:07:56 +0300

    example data removed

--- frontend_app/src/context/GlobalState.js
+++ frontend_app/src/context/GlobalState.js
@@ -4,11 +4,7 @@ import AppReducer from './AppReducer'
 // Initital state
 
 const initialState = {
-    refuels: [
-        { id: 1, text: 'Lohjan ABC 1.2.2023', amount: 45.2, pricePerLiter: 2.21, kilometersDriven: 587},
-        { id: 2, text: 'Liedon Neste 15.2.2023', amount: 43.5, pricePerLiter: 2.01, kilometersDriven: 551},
-        { id: 3, text: 'Raision ABC 30.2.2023', amount: 39.9, pricePerLiter: 1.85, kilometersDriven: 498}
-    ]
+    refuels: []
 }
 
 // Create context
